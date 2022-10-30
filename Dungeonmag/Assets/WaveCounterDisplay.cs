a7f2ce1d4a2ed8db61f477dff8187d6f4bcea75d
SeveredNode <ajlpersonal01@gmail.com>
2022-10-29 19:06:06 -0700

    fixed wave counter

--- Dungeonmag/Assets/WaveCounterDisplay.cs
+++ Dungeonmag/Assets/WaveCounterDisplay.cs
@@ -9,18 +9,29 @@ public class WaveCounterDisplay : MonoBehaviour
     public TMP_Text waveDisplay;
     public float waveNumber;
     public float projectileDamage;
+    public WaveCounter waveCounterScript;
 
     // Start is called before the first frame update
     void Start()
     {
         waveNumber = 1;
         waveDisplay.text = "Wave: " + waveNumber.ToString();
+        waveCounterScript = GameObject.Find("Player").GetComponent<WaveCounter>();
     }
     
     // Update is called once per frame
     void Update()
     {
+
+        if (waveCounterScript.enemiesKilled >= 5)
+        {
+            waveNumber = waveNumber + 1;
+            waveCounterScript.enemiesKilled = 0;
+        }
+
         waveDisplay.text = "Wave: " + waveNumber.ToString();
+
+
     }
 
 
